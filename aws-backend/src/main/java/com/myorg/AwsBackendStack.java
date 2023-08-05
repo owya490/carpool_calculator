6287d92ff3cc5b579e89eb890f721e4f989ae421
Owen <owen.yang@sowaustralia.com>
2023-08-05 21:19:55 +1000

    aws infra

--- aws-backend/src/main/java/com/myorg/AwsBackendStack.java
+++ aws-backend/src/main/java/com/myorg/AwsBackendStack.java
@@ -1,10 +1,17 @@
 package com.myorg;
 
 import software.constructs.Construct;
+import software.amazon.awscdk.AssetOptions;
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
 
 public class AwsBackendStack extends Stack {
     public AwsBackendStack(final Construct scope, final String id) {
@@ -20,5 +27,17 @@ public class AwsBackendStack extends Stack {
         // final Queue queue = Queue.Builder.create(this, "AwsBackendQueue")
         //         .visibilityTimeout(Duration.seconds(300))
         //         .build();
+
+        Function carpoolFunction = new Function(this, "carpoolFunction", FunctionProps.builder()
+        .runtime(Runtime.PYTHON_3_9)
+        .code(Code.fromAsset("./resources/"))
+        .handler("carpoolHandler.calculator")
+        .memorySize(1024)
+        .timeout(Duration.seconds(300))
+        .build());
+
+        LambdaRestApi.Builder.create(this, "carpoolEndpoint")
+        .handler(carpoolFunction)
+        .build();
     }
 }
